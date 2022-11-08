a4c12aae46737b41835bc003950d9f7c4c8a6216
guillemova <104459962+guillemova@users.noreply.github.com>
2022-11-08 14:56:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const button = document.querySelectorAll(".btn-burger")
-const menu = document.querySelector(".navbar");
-menu.addEventListener("click", function(){
-   menu.classList.toggle(".navbar__show-you")
+const buttonBurger = document.querySelector(".btn-burger")
+const menu = document.querySelector(".navbar ul");
+buttonBurger.addEventListener("click",() => {
+   console.log(menu.classList.toggle("navbar__show-you"))
+   // menu.classList.toggle("navbar__show-you")
 })
-
 
 
