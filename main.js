c2626b6b65beb1cf0aa8dacfa0f9dd6c58b6543c
guillemova <104459962+guillemova@users.noreply.github.com>
2022-11-08 15:32:25 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const buttonBurger = document.querySelector(".btn-burger")
-const menu = document.querySelector(".navbar ul");
-buttonBurger.addEventListener("click",() => {
-   console.log(menu.classList.toggle("navbar__show-you"))
-   // menu.classList.toggle("navbar__show-you")
-})
-
-
